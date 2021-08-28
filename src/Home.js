847f84f18a53bc576eb56a2d6ffe1860f0a940ae
angelinapoltavskiy <angelinapoltavskiy@gmail.com>
2021-08-28 11:57:29 -0700

    adding intro

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
+export default function Home() {
+  return (
+    <div>
+      <div class="center">
+        <img class="ann" src="./headshot.jpg" alt="a girl, Angelina, smiling" />
+      </div>
 
-export function Home() {
-
-     return (
-        <div>
-            <img class="ann" src="img/headshot.jpg" alt="a girl, Angelina, smiling" />
-        </div>
-     );
+      <div class="intro">
+        <h1>Angelina Poltavskiy</h1>
+        <p>Hello, welcome to my Portfolio!</p>
+        <p>
+          Just a bit about my self, I'm an aspiring Front End Developer,
+          currently in my senior year at UW studying Informatics.
+        </p>
+      </div>
+    </div>
+  );
 }
